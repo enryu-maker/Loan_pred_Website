c459b9c566af33e6e936d065fb60320403b3bd14
enryu-maker <akifkhan60067@gmail.com>
2022-02-02 08:55:25 +0530

    Initial

---
+++ Loan_pred/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Loan_pred project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Loan_pred.settings')
+
+application = get_asgi_application()
